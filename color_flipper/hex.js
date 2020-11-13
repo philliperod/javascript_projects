f8049104525dc5c703bf186902430ca3aa017d2f
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-13 11:06:37 -0600

    created the same const variables and click event listener for hex.html; inside the click listener, created a for loop to run 6 times to randomly choose a value in the hex array and store it into a variable which will generate a random color

--- color_flipper/hex.js
+++ color_flipper/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const button = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+document.addEventListener('click', () => {
+  let hexColor = '#';
+  for (let i = 0; i < 6; i++) {
+    let hexValue = Math.floor(Math.random() * hex.length);
+    hexColor += hex[hexValue];
+  }
+  document.body.style.background = hexColor;
+  color.textContent = hexColor;
+});
+
+// difference between this hex.html and index.html will be a for loop that will run 6 times to choose a random value in the hex array
