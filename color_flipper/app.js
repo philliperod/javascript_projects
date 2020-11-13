0adb3535e983cdce92dada68a787610d38e1cfdc
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-13 10:30:16 -0600

    copied hex.html into index.html; in app.js, created two variables that stored the DOM for btn id and color class; created a blank click event listener

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -1,1 +1,6 @@
 const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const specialButton = document.getElementById('btn');
+const colorChange = document.querySelector('.color');
+
+document.addEventListener('click');
